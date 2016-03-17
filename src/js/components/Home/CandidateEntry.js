ec67c84359680989113f2456efe36e9bf76cf4f3
Jimmy Gong <gong.jim@gmail.com>
2016-03-17 15:43:11 -0700

    Adds demoable home page

--- src/js/components/Home/CandidateEntry.js
+++ src/js/components/Home/CandidateEntry.js
@@ -1,11 +1,19 @@
 import React, { Component, PropTypes } from 'react';
+import { BarMetric } from 'react-simple-charts';
 
 class CandidateEntry extends Component {
   render() {
     const { name, positive, negative, volume, mostCommonHashtag } = this.props;
+    console.log(positive);
     return (
       <div className='candidate-entry'>
         {name}
+        <BarMetric 
+          metricName={''}
+          value={`${positive.percent}% positive`}
+          percent={positive.percent}
+          label={'sentiment'}
+        />
       </div>
     );
   }
