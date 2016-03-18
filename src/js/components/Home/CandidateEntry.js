032e3d8e389e1d1f1b2bc8c3483b15c003a1c8fe
Jimmy Gong <gong.jim@gmail.com>
2016-03-17 19:29:57 -0700

    Refactor class name for icons in candidateEntry, touches up spacing

--- src/js/components/Home/CandidateEntry.js
+++ src/js/components/Home/CandidateEntry.js
@@ -1,17 +1,17 @@
 import React, { Component, PropTypes } from 'react';
 import BarMetric from '../BarMetric';
-import Clinton from '../../../images/Clinton.png';
 
 class CandidateEntry extends Component {
   render() {
     const { name, positive, negative, volume, mostCommonHashtag } = this.props;
+    const iconClass = `candidate-icon ${name.split(' ')[1].toLowerCase()}-icon`
     return (
       <div className='candidate-entry'>
         <div className='name-bar-wrapper'>
           <span className='candidate-name'>{ name }</span>
           <BarMetric fillPercent={ positive.percent } />
         </div>
-        <div className={name.split(' ')[1].toLowerCase()+'-icon'} />
+        <div className={ iconClass } />
       </div>
     );
   }
